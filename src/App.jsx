07ec981d89c49e2fd464369f8e88e83a6c682476
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-08-07 12:03:20 +0500

    Added full project files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Navigation from './components/Navigation';
+import Herosection from './components/Hero';
+function App() {
+  return (
+    <div>
+      <Navigation /> 
+      <Herosection/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div>
+        <nav className='container'>
+            <div className='logo'>
+                <img src="/images/brand_logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>login</button>
+        </nav>
+    </div>
+  )
+}
+
+export default Navigation
